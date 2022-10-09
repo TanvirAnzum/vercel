4f31be1ad43adb6e8071267fd29b6354d7a2eefd
TanvirAnzum <tanviranzum14@gmail.com>
2022-10-09 14:30:40 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://project-management-app-server.onrender.com/projects")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className="App">
+      {data && data.map((project) => <li key={project.id}>{project.title}</li>)}
+    </ul>
   );
 }
 
